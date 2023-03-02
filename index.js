18d28fe92381855d9e0304fff539203f208b12c2
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-01 20:23:10 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+You are a web developer for an accounting company. One thing you often have to deal with are the really big expenses of major customers. So your boss would like you to write an algorithm that, when given a dataset of expenses made, can find the most expensive expense in the list. The problem is that these datasets are not sorted at all (since your customers buy things in a random order). 
+
+Write a function called highestInArray that, when given an array of numbers, where each number represents an expense made, can find the one highest number in the array. 
+
+If your function is given an array with one or less numbers, your function should return the following string: "Your array does not contain enough data."
+
+Note that the array argument is NOT sorted when given to you! 
+
+For example:
+If your function was given the array like: [100, 50, 30, 90, 40, 150, 80, 300, 60, 55]
+Your function would return 300, because that is the highest number in the array. 
+
+*/
+
+// Write your code below: 
